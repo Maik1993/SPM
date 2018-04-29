91bcc73467de7a49d4114b0fa29cc7177cf6fbf4
Ina <Ina@user.fritz.box>
2018-04-29 21:35:39 +0200

    Session hinzugefuegt => Login fertig

--- WebContent/startseite.jsp
+++ WebContent/startseite.jsp
@@ -1,3 +1,12 @@
+<% 	
+ 	String username = (String) session.getAttribute("name"); 
+	System.out.println(username);
+ 	
+	if(username == null){
+		response.sendRedirect("login.jsp");
+ 	}
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +21,43 @@
 </head>
 <body>
 
-<% String passwort = request.getParameter("passwort"); %>
-<a>Welcome   <% out.println(passwort); %> User!!!! You have logged in.</a>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div id="image">
+					<img id="logo_img" src="images/Logo_transparent.png">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div id="ueberschrift">
+					<h1>Willkommen User <%= username %></h1>
+				</div>			
+			</div>
+			<div class="col-md-3">
+				<form action="Logout" method="POST">
+					<input type="submit" id="login_button" value="Logout" class="btn btn-primary btn-lg btn-block login-button">
+				</form>
+			</div>		
+		</div>
+		<br/>
+		<br/>
+	</div>
+
+
+	
+	
+	<footer>
+			<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<p>&copy; Just Do IT</p>
+					<p>David Boes, Christopher Heyn, Justine L&uuml;ken, Lars Lipport, Maik Habben, Ina Krefting</p>
+				</div>
+				<div class="col-md-3">
+					<img id="logo_gruppe_img" src="images/Just_do_it_logo.png">
+				</div>
+				</div>
+			</div>
+	</footer>
 </body>
 </html>
--- src/servlet/Logout.java
+++ src/servlet/Logout.java
@@ -1,25 +1,25 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.RequestDispatcher;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class Logout
  */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public Logout() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,7 +29,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+
 	}
 
 	/**
@@ -37,27 +37,9 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String passwort = request.getParameter("passwort");
-		String passwort_right = "123";
-		String address = "";
-		String user = "KaufDort";
-		
-		if(!passwort.equals(passwort_right))
-		{
-			address = "login.jsp";
-
-		} else
-		{
-			address = "startseite.jsp";
-			HttpSession session = request.getSession(true); 
-			session.setAttribute("name", user);
-
-		}
-		
-		request.setAttribute("passwort", passwort);
-		RequestDispatcher req = request.getRequestDispatcher(address);
+		request.getSession().invalidate();
+		RequestDispatcher req = request.getRequestDispatcher("login.jsp");
 		req.forward(request, response);
-		
 	}
 
 }
